4bdc24214ed200ccbe8f7686959e85e78953f857
pratiklodha <lodhapratik9@gmail.com>
2022-05-08 17:30:14 +0530

    desc updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -73,7 +73,7 @@ function NavBar() {
             <Nav.Item>
               <Nav.Link
                 as={Link}
-                to="/project"
+                to="/dream"
                 onClick={() => updateExpanded(false)}
               >
                 <AiOutlineRocket
